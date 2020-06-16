55c7ca0ea23d4a1ecbddc40bbc4400ad168a8544
CynthiaKamau <cynthiakamau54@gmail.com>
2020-06-16 23:17:41 +0300

    Bug fixes

--- app/Http/Controllers/C4cJitengeUssdController.php
+++ app/Http/Controllers/C4cJitengeUssdController.php
@@ -109,14 +109,13 @@ class C4cJitengeUssdController extends Controller
                                         'password' => $password
 									],
 									'cookies' => false
-									]
+                                    ]
+                                    
                             );                               
 								
-                            $response = json_decode($response->getBody());    
-															
+                            $response = json_decode($response->getBody());
+                            															
 							if (!empty($response) && $response->success === true) {
-
-                                dd($response);
 																											
 								$session['token'] = $response->access_token;
 									
@@ -130,16 +129,48 @@ class C4cJitengeUssdController extends Controller
                                 $response = "CON C4C\nDownload the C4C App and complete your profile";
 
                                 $this->deleteSession($session);
+
                             } 
                                 
                             break;
 
-						
-					}	
+                            case 3: 
+                                $response = $this->performEvaluation($session, $parts);
+
+                            break;
+
+                            case 4: 
+                                $response = $this->performEvaluation($session, $parts);
+
+                            break;
+
+                            case 5: 
+                                $response = $this->performEvaluation($session, $parts);
+
+                            break;
+
+                            case 6: 
+                                $response = $this->performEvaluation($session, $parts);
+
+                            break;
+
+                            case 7: 
+                                $response = $this->performEvaluation($session, $parts);
+
+                            break;
+
+                            default: 
+
+                                $response = $this->performEvaluation($session, $parts);
+
+                            break;
 				
+
+                        }
+                           
 				
 				//return $response;
-			}
+			        }
 			
 			return response($response, 200)->header("Content-Type", "text/plain");
 		}
@@ -160,7 +191,7 @@ class C4cJitengeUssdController extends Controller
 
                 $session["contact_person"] = $parts[2] == "1" ? "YES" : "NO";
 
-               $this->setSession($session);
+               //$this->setSession($session);
 
                 $response = "CON C4C\nWhen did you get into contact with someone with COVID 19? DDMMYYYY"; 
 
@@ -168,9 +199,9 @@ class C4cJitengeUssdController extends Controller
 
             case 4:
                 
-                if (strlen($parts[4]) != 8) {
+                if (strlen($parts[3]) != 8) {
 								
-                    unset($session[4]);
+                    unset($session[3]);
                     
                     $response = "CON C4C\nWhen did you get into contact with someone with COVID 19 DDMMYYYY";
                     
@@ -178,15 +209,13 @@ class C4cJitengeUssdController extends Controller
                     
                     try {
                         
-                        $session['contact_date'] = Carbon::createFromFormat('dmY', $parts[4])->format('Y-m-d');
-                        
-                        $this->setSession($session);
-                        
+                        $session['contact_date'] = Carbon::createFromFormat('dmY', $parts[3])->format('Y-m-d');
+                                                
                         $response = "CON C4C\nDid you receive IPC training?\n1. Yes\n2. No";
                         
                     } catch (Exception $exception) {
                         
-                        unset($session[4]);
+                        unset($session[3]);
                         
                         $response = "CON C4C\nWhen did you get into contact with someone with COVID 19?";
                         
@@ -197,11 +226,9 @@ class C4cJitengeUssdController extends Controller
 
             case 5:
 
-                if ($parts[5] == "1" || $parts[5] == "2") {
+                if ($parts[4] == "1" || $parts[4] == "2") {
 
                     $session["ipc_training"] = $parts[4] == "1" ? "YES" : "NO";
-
-                    $this->setSession($session);
 
                     $response = "CON C4C\nDuring interaction with a COVID-19 patient, were you wear personal protective equipment (PPE)?\n1. Yes \n2. No";
              
@@ -217,13 +244,11 @@ class C4cJitengeUssdController extends Controller
 
             case 6:
 
-                if ($parts[6] == "1" || $parts[6] == "2") {
+                if ($parts[5] == "1") {
 
-                    $session["ppe_worn"] = $parts[6] == "1" ? "YES" : "NO";
+                    $session["ppe_worn"] = $parts[5] == "1" ? "YES" : "NO";
 
-                    $this->setSession($session);
-
-                    if ($parts[6] == "1")
+                    if ($parts[5] == "1")
 
                         $response = "CON C4C\nDuring interaction with a COVID-19 patient, did you wear personal protective equipment (PPE)?\n1. Single Gloves\n2. N95 mask (or equivalent)\n3. Face shield or goggles/protective glasses\n4. Disposable gown\n5. Waterproof apron\n6. None";
              
@@ -231,7 +256,7 @@ class C4cJitengeUssdController extends Controller
 
                         $response = "CON C4C\nYou have entered an invalid response. Try again.";
 
-                         $this->deleteSession($session);
+                        $this->deleteSession($session);
 
                     }
 
@@ -239,11 +264,9 @@ class C4cJitengeUssdController extends Controller
 
             case 7:
 
-                if ($parts[7] == "1" || $parts[7] == "2") {
+                if ($parts[6] == "1" || $parts[6] == "2") {
 
-                    $session["ppe_worn"] = $parts[7] == "1" ? "YES" : "NO";
-
-                    $this->setSession($session);
+                    $session["ppe_worn"] = $parts[6] == "1" ? "YES" : "NO";
 
                     $response = "CON C4C\nWhich of these symptoms are you experiencing any of these symptoms?\n1. Fever\n2. Cough\n3. Difficulty in breathing\n4. Fatigue\n5. Sneezing\n6. Sore throat\n7. None";
 
@@ -254,6 +277,12 @@ class C4cJitengeUssdController extends Controller
 
                 $this->deleteSession($session);
             }
+
+            break;
+
+            default:
+
+            $response = "END Thank you for reporting an exposure. Your responses have been recorded";
 
             break;
 
